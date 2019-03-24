58c40ef5fcbf969a4dc58d7f971443cd17647de5
be222dd <beysimeryilmaz@gmail.com>
2019-03-24 07:37:10 +0100

    Some error handling and logging added

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 const productRoutes = require('./api/routes/products')
 const orderRoutes = require('./api/routes/orders')
 
+app.use(morgan('dev'))
 app.use('/products', productRoutes)
 app.use('/orders', orderRoutes)
+
+// error handler custom 404 error
+app.use(function (req, res, next) {
+  const error = new Error('Not Found')
+  error.status = 404
+  next(error)
+})
+
+//any other error thrown
+app.use(function (error, req, res, next) {
+  res.status(error.status || 500)
+  res.json({
+    error: {
+      message: error.message
+    }
+  })
+})
 
 module.exports = app
